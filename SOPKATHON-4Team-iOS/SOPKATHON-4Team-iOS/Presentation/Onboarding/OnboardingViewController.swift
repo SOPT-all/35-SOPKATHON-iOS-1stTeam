047330859005021bb94019d7b3d957a45dc32b3d
cirtuare <cirtuare@gmail.com>
2024-11-24 00:56:39 +0900

    [Feat] #9 - OnboardingViewController UI 구현 완성

--- SOPKATHON-4Team-iOS/SOPKATHON-4Team-iOS/Presentation/Onboarding/OnboardingViewController.swift
+++ SOPKATHON-4Team-iOS/SOPKATHON-4Team-iOS/Presentation/Onboarding/OnboardingViewController.swift
@@ -19,9 +19,9 @@ class OnboardingViewController : UIViewController {
     var startButton: UIButton = UIButton()
     
     override func viewDidLoad() {
+        setStyle()
         setHierarchy()
         setLayout()
-        setStyle()
     }
     
     func setHierarchy() {
@@ -43,22 +43,45 @@ class OnboardingViewController : UIViewController {
         }
         
         startButton.snp.makeConstraints {
-            $0.top.equalToSuperview().offset(418)
-            $0.horizontalEdges.equalToSuperview().inset(48)
+            $0.bottom.equalToSuperview().inset(46)
+            $0.horizontalEdges.equalToSuperview().inset(26)
             $0.height.equalTo(52)
         }
     }
     
     func setStyle() {
-        nicknameLabel = LabelFactory.build(font: <#T##UIFont#>)
+        view.backgroundColor = UIColor(resource: .background)
+        
+        nicknameLabel = LabelFactory.build(text: "닉네임을 입력하세요",
+                                           font: .titleB24,
+                                           textColor: .black,
+                                           textAlignment: .center)
         
         nicknameTextField.do {
             $0.backgroundColor = .white
+            $0.textAlignment = .center
+            $0.font = .bodyR14
         }
         
         startButton.do {
             $0.setTitle("시작하기", for: .normal)
+            $0.setTitleColor(.white, for: .normal)
+            $0.titleLabel?.font = .bodyB18
+            $0.backgroundColor = UIColor(resource: .orange)
+            $0.layer.cornerRadius = 8
+            $0.addTarget(self, action: #selector(startButtonTapped), for: .touchUpInside)
         }
+    }
+    
+    @objc
+    func startButtonTapped() {
+        // 홈 화면으로 이동
+        postNickname()
+    }
+    
+    func postNickname() {
+        let nickname = nicknameTextField.text ?? ""
+        // 닉네임 서버 Post 통신
     }
     
 }
