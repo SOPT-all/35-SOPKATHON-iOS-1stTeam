f78269b34a793016e66cb061bc62e41bdc433bb6
sem-git <lsmsm0327@naver.com>
2024-11-24 05:12:37 +0900

    [Design] #29 - 스플래쉬 뷰 글자 로고 이미지 추가

--- SOPKATHON-4Team-iOS/SOPKATHON-4Team-iOS/Presentation/Splash/SplashViewController.swift
+++ SOPKATHON-4Team-iOS/SOPKATHON-4Team-iOS/Presentation/Splash/SplashViewController.swift
@@ -13,14 +13,22 @@ import Then
 class SplashViewController : UIViewController {
     
     let splashImageView = UIImageView()
+    let splashNameImageView = UIImageView()
     
     override func viewDidLoad() {
-        view.addSubview(splashImageView)
+        view.addSubviews(splashImageView, splashNameImageView)
         
         splashImageView.snp.makeConstraints {
-            $0.centerX.centerY.equalToSuperview()
+            $0.top.equalToSuperview().offset(243)
+            $0.centerX.equalToSuperview()
             $0.width.equalTo(214)
             $0.height.equalTo(243)
+        }
+        
+        splashNameImageView.snp.makeConstraints {
+            $0.top.equalTo(splashImageView.snp.bottom)
+            $0.centerX.equalToSuperview()
+            $0.width.height.equalTo(143)
         }
         
         view.backgroundColor = UIColor(resource: .background)
@@ -30,10 +38,16 @@ class SplashViewController : UIViewController {
             $0.contentMode = .scaleAspectFill
         }
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) { [weak self] in
-            let onboardingVC = OnboardingViewController()
-            self?.navigationController?.pushViewController(onboardingVC, animated: false)
+        splashNameImageView.do{
+            $0.image = UIImage(resource: .splashNameLogo)
+            $0.clipsToBounds = true
+            $0.contentMode = .scaleAspectFill
         }
+        
+        //        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) { [weak self] in
+        //            let onboardingVC = OnboardingViewController()
+        //            self?.navigationController?.pushViewController(onboardingVC, animated: false)
+        //        }
     }
     
 }
