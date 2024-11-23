dcd8dc3ffb8fb6aa3441d0672ec3e1414db691fa
cirtuare <cirtuare@gmail.com>
2024-11-24 04:04:51 +0900

    [Feat] #14 - UIStackView+ extension 구현

---
+++ SOPKATHON-4Team-iOS/SOPKATHON-4Team-iOS/Global/Extension/UIKit+/UIStackView+.swift
@@ -0,0 +1,27 @@
+//
+//  UIStackView+.swift
+//  SOPKATHON-4Team-iOS
+//
+//  Created by 이수민 on 11/24/24.
+//
+
+import UIKit
+
+extension UIStackView {
+    
+    func addArrangedSubviews(_ views: [UIView]) {
+        views.forEach { self.addArrangedSubview($0) }
+    }
+    
+    func setStackView(axis: NSLayoutConstraint.Axis = .horizontal,
+                      distribution: UIStackView.Distribution = .fillEqually,
+                      alignment: UIStackView.Alignment = .fill,
+                      spacing: CGFloat = 0) {
+        self.axis = axis
+        self.distribution = distribution
+        self.alignment = alignment
+        self.spacing = spacing
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
+
+}
