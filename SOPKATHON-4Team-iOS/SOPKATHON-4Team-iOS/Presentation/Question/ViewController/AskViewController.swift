6c43bf0e02e55d61de981927ff67ace39dac68f0
cirtuare <cirtuare@gmail.com>
2024-11-24 05:29:50 +0900

    [Feat] #14 - QusetionVC들 세팅

--- SOPKATHON-4Team-iOS/SOPKATHON-4Team-iOS/Presentation/Question/ViewController/AskViewController.swift
+++ SOPKATHON-4Team-iOS/SOPKATHON-4Team-iOS/Presentation/Question/ViewController/AskViewController.swift
@@ -5,3 +5,26 @@
 //  Created by 이수민 on 11/24/24.
 //
 
+import UIKit
+
+import SnapKit
+import Then
+
+class AskViewController: UIViewController {
+    
+    lazy var askContentView: QuestionContentView = {
+        return QuestionContentView(questionType: .ask)
+    }()
+    
+    // MARK: - View Life Cycless
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.addSubview(askContentView)
+        askContentView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+}
--- SOPKATHON-4Team-iOS/SOPKATHON-4Team-iOS/Presentation/Question/ViewController/SolveViewController.swift
+++ SOPKATHON-4Team-iOS/SOPKATHON-4Team-iOS/Presentation/Question/ViewController/SolveViewController.swift
@@ -5,3 +5,26 @@
 //  Created by 이수민 on 11/24/24.
 //
 
+import UIKit
+
+import SnapKit
+import Then
+
+class SolveViewController: UIViewController {
+    
+    lazy var askContentView: QuestionContentView = {
+        return QuestionContentView(questionType: .solve)
+    }()
+    
+    // MARK: - View Life Cycless
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.addSubview(askContentView)
+        askContentView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+}
